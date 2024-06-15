59f9f4b1b0767e5d5ccf30c773baaaac3f8a7c48
orps23124137 <orps23124137@gmail.com>
2024-06-15 15:23:51 +0800

    search untested, others are complete

--- database/app.py
+++ database/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 import jnius_config
 jnius_config.add_classpath(".;sqlite-jdbc-3.45.3.0.jar;slf4j-api-1.7.36.jar")
 from jnius import autoclass, JavaClass, MetaJavaClass
 
 DiaryManager = autoclass("DiaryManager")
+Pair = autoclass("Pair")
 DB = DiaryManager()
 String = autoclass('java.lang.String')
 ArrayList = autoclass("java.util.ArrayList")
 JavaArray = autoclass('java.lang.reflect.Array')
+HashMap = autoclass("java.util.HashMap")
 
 
 
@@ -17,50 +19,95 @@ app = Flask(__name__)
 def makeDB():
     DB.makeDB()
     print("makeDB")
-    return ""
+    resp = make_response("database is created or already created")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 @app.route("/resetDB", methods=["POST"])
 def resetDB():
     DB.resetDB()
     print("resetDB")
-    return ""
+    resp = make_response("database is reset")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 @app.route("/resetSettings", methods=["POST"])
 def resetSettings():
     DB.resetSettings()
     print("resetSettings")
-    return ""
+    resp = make_response("settings are reset")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 @app.route("/saveDiary", methods = ["POST"])
 def saveDiary():
-    DB.saveDiary(request.values["title"], request.values["content"])
+    tags = ArrayList()
+    for i in request.values["tags"].split(" "):
+        tags.add(i)
+    DB.saveDiary(request.values["title"], request.values["content"], tags)
     print("saved Diary {}".format(request.values["title"]))
-    return ""
+    return "diary saved"
 
 @app.route("/saveEvent", methods = ["POST"])
 def saveEvent():
-    DB.saveEvent(request.values["title"], request.values["content"], request.values["endTime"])
-    print("saved Diary {}".format(request.values["title"]))
-    return ""
+    tags = ArrayList()
+    for i in request.values["tags"].split(" "):
+        tags.add(i)
+    DB.saveEvent(request.values["title"], request.values["content"], request.values["endTime"], tags)
+    print("saved event {}".format(request.values["title"]))
+    return "event saved"
+
+""" 
+@app.route("/saveSchedule", methods=["POST"])
+def saveSchedule():
+    tags = ArrayList()
+    for i in request.values["tags"].split(" "):
+        tags.add(i)
+    DB.saveSchedule(request.values["title"], request.values["content"], request.values["endTime"], tags)
+    print("saved schedule {}".format(request.values["title"]))
+    return "Schedule saved" """
 
 @app.route("/listRecord", methods = ["GET"])
 def listRecord():
-    return list(map(lambda x:dict(x.entrySet()), DB.listRecord()))
+    resp = make_response(list(map(lambda x:dict(x.entrySet()), DB.listRecord())))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
     
 
 @app.route("/deleteRecord", methods = ["POST"])
 def deleteRecord():
-    DB.deleteRecord(request.values["id"])
-    return ""
+    DB.deleteRecord(int(request.values["id"]))
+    return "record is deleted"
 
 @app.route("/editRecord", methods = ["POST"])
+#maybe make one that doesn't require everything
 def editRecord():
-    #DB.editRecord(request.values["id"], request.values["content"])
-    return ""
+    tags = ArrayList()
+    for i in request.values["tags"].split(" "):
+        tags.add(i)
+    DB.editRecord(int(request.values["id"]), request.values["endTime"], request.values["title"], request.values["content"], tags)
+    return "record is edited"
 
 @app.route("/searchRecord", methods=["GET"])
+
 def searchRecord():
-    return ""
+    conditions = HashMap()
+    for condition, value in request.values["conditions"]:
+        conditions.put(condition, value)
+    
+    tags = ArrayList()
+    for i in request.values["tags"].split(" "):
+        tags.add(i)
+    
+    toreturn = ArrayList()
+    for r in request.form.getlist("query"):
+        toreturn.add(r)
+
+    orders = ArrayList()
+    for order, value in request.values["orders"]:
+        orders.put(Pair(order, value))
+
+    return list(map(lambda x:dict(x.entrySet()), DB.searchRecord(conditions, tags, toreturn, request.values["limit"], orders)))
 
 @app.route("/getRecordAll", methods=["GET"])
 def getRecordAll():
@@ -83,26 +130,34 @@ def getRecordCustom():
 
 @app.route("/tagList", methods = ["GET"])
 def tagList():
-    return list(map(lambda x: (x.first, x.second), list(DB.tagList())))
+    resp = make_response(list(map(lambda x: (x.first, x.second), list(DB.tagList()))))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 @app.route("/tags", methods = ["GET"])
 def tags():
-    return list(map(lambda x: (x.first, x.second), list(DB.tags())))
+    resp = make_response(list(map(lambda x: (x.first, x.second), list(DB.tags()))))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 @app.route("/setSetting", methods = ["POST"])
 def setSetting():
     DB.setSetting(request.values["setting"], request.values["value"])
     print("set setting {} : {}".format(request.values["setting"], request.values["value"]))
-    return ""
+    return "{} is set to {}".format(request.values["setting"], request.values["value"])
 
-@app.route("/getSetting", methods = ["GET"])
+@app.route("/getSetting", methods = ["POST"])
 def getSetting():
-    setting = DB.getSetting(request.values["setting"])
-    print("get setting {}".format(setting))
-    return setting
+    #print(request)
+    print(request.values["setting"])
+    resp = make_response(DB.getSetting(request.values["setting"]))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    #print("get setting {}".format(setting))
+    return resp
 
 @app.route("/getSettings", methods = ["GET"])
 def getSettings():
-    settingMap = dict(DB.getSettings())
-    print(settingMap)
-    return settingMap
+    resp = make_response(dict(DB.getSettings().entrySet()))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    #print(settingMap)
+    return resp
