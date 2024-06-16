b7feed857feb1747dfb5532db6d64393cb535ad6
orps23124137 <orps23124137@gmail.com>
2024-06-16 16:45:14 +0800

    changed structure of codes and made a exe

--- database/test.py
+++
@@ -1,9 +0,0 @@
-#import jnius_config
-#jnius_config.add_classpath(".;sqlite-jdbc-3.45.3.0.jar;slf4j-api-1.7.36.jar")
-from jnius import autoclass
-Test = autoclass("Test")
-ArrayList = autoclass("java.util.ArrayList")
-a = ArrayList()
-a.add("hello")
-a.add("world")
-Test.say(a)
